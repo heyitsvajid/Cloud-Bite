a83b359aa967bcdc2ebb232cab860e9ade127671
Vajid Kagdi <vajid.kagdi@sjsu.edu>
2018-11-19 01:10:47 -0800

    Tenant GO-Mongo APIs initial project setup
    GO-Mongo APIs initial project setup along with GET and GET all tenant details APIs

---
+++ Backend/go-tenant-apis/src/tenant/server.go
@@ -0,0 +1,123 @@
+/*
+Base Project Reference : https://github.com/paulnguyen/cmpe281/tree/master/golabs/godata/go-gumball-mongo
+*/
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/codegangsta/negroni"
+	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+// MongoDB Config
+var mongodb_server = "mongodb://cmpe281:cmpe281@ds051007.mlab.com:51007/saas"
+var mongodb_database = "saas"
+var mongodb_collection = "tenant"
+
+// NewServer configures and returns a Server.
+func NewServer() *negroni.Negroni {
+	formatter := render.New(render.Options{
+		IndentJSON: true,
+	})
+	n := negroni.Classic()
+	mx := mux.NewRouter()
+	initRoutes(mx, formatter)
+	n.UseHandler(mx)
+	return n
+}
+
+// API Routes
+func initRoutes(mx *mux.Router, formatter *render.Render) {
+	mx.HandleFunc("/ping", pingHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/tenant/{id}", tenantHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/tenant", tenantUpdateHandler(formatter)).Methods("PUT")
+	mx.HandleFunc("/tenant", tenantNewEntryHandler(formatter)).Methods("POST")
+	mx.HandleFunc("/tenants", tenantAllHandler(formatter)).Methods("GET")
+}
+
+// Helper Functions
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
+// API Ping Handler
+func pingHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		formatter.JSON(w, http.StatusOK, struct{ Test string }{"API version 1.0 alive!"})
+	}
+}
+
+// API Get Tenant Details
+func tenantHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		session, err := mgo.Dial(mongodb_server)
+		if err != nil {
+			panic(err)
+		}
+		defer session.Close()
+		session.SetMode(mgo.Monotonic, true)
+		params := mux.Vars(req)
+		var id string = params["id"]
+		fmt.Println("Order ID: ", id)
+		var result bson.M
+		if id == "" {
+			formatter.JSON(w, http.StatusBadRequest, "Tenant ID Missing")
+		} else {
+			c := session.DB(mongodb_database).C(mongodb_collection)
+			err = c.Find(bson.M{"_id": id}).One(&result)
+			if err != nil {
+			fmt.Println(" Tenant: ", err)
+			formatter.JSON(w, http.StatusBadRequest, "Not Found")
+			}
+			fmt.Println(" Tenant: ", result)
+			formatter.JSON(w, http.StatusOK, result)
+		}
+	}
+}
+
+// API Get All Tenant Details
+func tenantAllHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		session, err := mgo.Dial(mongodb_server)
+		if err != nil {
+			panic(err)
+		}
+		defer session.Close()
+		session.SetMode(mgo.Monotonic, true)
+		var result bson.M
+		c := session.DB(mongodb_database).C(mongodb_collection)
+		err = c.Find(bson.M{}).One(&result)
+		if err != nil {
+			fmt.Println(" Tenant: ", err)
+			formatter.JSON(w, http.StatusBadRequest, "Not Found")
+		}
+		fmt.Println("All Tenants:", result)
+		formatter.JSON(w, http.StatusOK, result)
+		
+	}
+}
+
+// API Update Tenant Details
+func tenantUpdateHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintln(w, "not implemented yet !")
+	}
+}
+
+
+// API Create Tenant
+func tenantNewEntryHandler(formatter *render.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintln(w, "not implemented yet !")
+	}
+}
+
