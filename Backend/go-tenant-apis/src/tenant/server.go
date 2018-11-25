ce26f47737ceae8cd265c9749028168fef1cf4b4
MURTAZA <murtazamanasawala110@gmail.com>
2018-11-25 14:13:57 -0800

    Update Tenant API

--- Backend/go-tenant-apis/src/tenant/server.go
+++ Backend/go-tenant-apis/src/tenant/server.go
@@ -34,12 +34,6 @@ func NewServer() *negroni.Negroni {
 	mx := mux.NewRouter()
 	initRoutes(mx, formatter)
 	n.UseHandler(mx)
-	// c := cors.New(cors.Options{
-	//     AllowedOrigins: []string{"*"},
-	//     AllowedHeaders: []string{"Content-Type"},
-	//     AllowedMethods: []string{"GET", "POST", "PATCH","PUT", "DELETE, OPTIONS"},
-	// })
-	// n.Use(c)
 	return n
 }
 
@@ -141,7 +135,36 @@ func tenantAllHandler(formatter *render.Render) http.HandlerFunc {
 // API Update Tenant Details
 func tenantUpdateHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintln(w, "not implemented yet !")
+		setupResponse(&w, req)
+	
+		if (*req).Method == "OPTIONS" {
+			fmt.Println("PREFLIGHT Request")
+			return
+		}
+    	var m Tenant
+    	_ = json.NewDecoder(req.Body).Decode(&m)		
+
+    	fmt.Println("Update Tenant Products To: ", m.Products)
+		session, err := mgo.Dial(mongodb_server)
+        if err != nil {
+                panic(err)
+        }
+        defer session.Close()
+        session.SetMode(mgo.Monotonic, true)
+        c := session.DB(mongodb_database).C(mongodb_collection)
+        query := bson.M{"id" : m.ID}
+        change := bson.M{"$set": bson.M{ "products" : m.Products}}
+        err = c.Update(query, change)
+        if err != nil {
+                log.Fatal(err)
+        }
+       	var result bson.M
+        err = c.Find(bson.M{"id" : m.ID}).One(&result)
+        if err != nil {
+                log.Fatal(err)
+        }        
+        fmt.Println("Tenant Products Updated", result )
+		formatter.JSON(w, http.StatusOK, result)
 	}
 }
 
@@ -182,4 +205,3 @@ func tenantNewEntryHandler(formatter *render.Render) http.HandlerFunc {
 
 	}
 }
-
