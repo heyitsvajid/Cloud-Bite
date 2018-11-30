8a891fb454fd21e42b7bb2b1079d753d45a8939e
MURTAZA <murtazamanasawala110@gmail.com>
2018-11-29 18:46:16 -0800

    Design Updates for user client

--- Frontend/user-client/src/index.js
+++ Frontend/user-client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LogIn from './Components/LogIn';
 import SignUp from './Components/Signup';
+import Menu from './Components/Menu';
 
 
 ReactDOM.render(
@@ -11,6 +12,7 @@ ReactDOM.render(
 
         <Route exact path="/" component={LogIn} />
         <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/menu" component={Menu} />
       </div>
     </Router>,
   document.getElementById('root')
