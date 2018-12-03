8ede19f79ebeb9b0f371ee877b6e5c834756ac92
sricheta92 <srichetaruj.iste@gmail.com>
2018-12-02 18:18:21 -0800

    Added KONg API Gateway

--- API's/Riak-API-Test/server.go
+++ API's/Riak-API-Test/server.go
@@ -57,3 +57,55 @@ func init() {
 	fmt.Println("Riak  Server3:", server3 )	
 
 }
+
+
+func main() {
+	phone := "14158586273"
+	// QueryEscape escapes the phone string so
+	// it can be safely placed inside a URL query
+	burger := url.QueryEscape(burger)
+
+
+
+	// Build the request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		return
+	}
+
+	// For control over HTTP client headers,
+	// redirect policy, and other settings,
+	// create a Client
+	// A Client is an HTTP client
+	client := &http.Client{}
+
+	// Send the request via a client
+	// Do sends an HTTP request and
+	// returns an HTTP response
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		return
+	}
+
+	// Callers should close resp.Body
+	// when done reading from it
+	// Defer the closing of the body
+	defer resp.Body.Close()
+
+	// Fill the record with the data from the JSON
+	var record Numverify
+
+	// Use json.Decode for reading streams of JSON data
+	if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println("PostBurgerOrder No. = ", record.PostBurgerOrder)
+	fmt.Println("type   = ", record.type)
+	fmt.Println("price  = ", record.price)
+	fmt.Println("filling   = ", record.filling)
+	fmt.Println("sauces  = ", record.sauces)
+
+}
